981eb1002f5b5bb0911f10609ccfc6fd56ca6b2b
Dennis Alexandru <playschan@gmail.com>
2022-11-19 12:00:44 +0200

    initial commit

---
+++ OOP-Training/OOP-Training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
